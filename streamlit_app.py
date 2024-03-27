e68bfb83112dc7de3716a6699bfa10bf31889fd9
pnzx <162673756+pnzx@users.noreply.github.com>
2024-03-27 01:05:20 +0000

    차트만들기

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-st.title("this is the app title")
-st.markdown("this is markdown")
-st.header("this is header")
-st.subheader("this is subheader")
-st.caption("this is caption")
-st.code("x=2021")
-st.latex(r'''
-         a + b r^2 + a r^3 ''')
+rand = np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
